a939c208c256dbfb26be4ba319de21beb3acd3c0
Shen Liao <shen.liao@thoughtworks.com>
2020-08-11 19:34:26 +0800

    [Shen Liao]: finish 3-object exercise

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,9 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  const serialObject = {
+    serialNumber: '12345'
+  };
+  const result = Object.assign(serialObject, source);
+  result.properties.status = 'processed';
+  return result;
 }
--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,8 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  return Object.values(source).reduce((total, num) => {
+    // eslint-disable-next-line no-param-reassign
+    total += Number(num);
+    return total;
+  }, 0);
 }
