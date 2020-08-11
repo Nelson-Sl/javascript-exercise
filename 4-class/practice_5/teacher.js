d619f714f73718f03823f048b999fe6bb3dd8233
Shen Liao <shen.liao@thoughtworks.com>
2020-08-11 21:29:25 +0800

    [Shen Liao]: finish 4-class exercise

--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,15 @@
 // TODO 20: 在这里写实现代码
+import Person from './person';
+
+export default class Teacher extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    return this.klass === undefined
+      ? super.introduce().concat(` I am a Teacher. I teach No Class.`)
+      : super.introduce().concat(` I am a Teacher. I teach Class ${this.klass}.`);
+  }
+}
